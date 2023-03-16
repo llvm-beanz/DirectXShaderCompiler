b550c6fb7a595d5b2db7bd36aeb891ca161cfe3e
Chris Bieneman <cbieneman@microsoft.com>
2023-03-16 16:09:20 -0500

    Don't run the parameter legalizaiton pass
    This should all be done at the AST level

--- include/dxc/HLSL/DxilGenerationPass.h
+++ include/dxc/HLSL/DxilGenerationPass.h
@@ -125,9 +125,6 @@ void initializeCleanupDxBreakPass(llvm::PassRegistry &);
 FunctionPass *createDxilLoopDeletionPass(bool NoSink);
 void initializeDxilLoopDeletionPass(llvm::PassRegistry &);
 
-ModulePass *createHLLegalizeParameter();
-void initializeHLLegalizeParameterPass(llvm::PassRegistry &);
-
 bool AreDxilResourcesDense(llvm::Module *M,
                            hlsl::DxilResourceBase **ppNonDense);
 
