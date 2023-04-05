da2dacbe2a28144d0788182135641230ef343a64
Chris Bieneman <cbieneman@microsoft.com>
2023-04-05 09:43:18 -0500

    Don't elide non-local storage

--- tools/clang/lib/Sema/HLSLOutParamBuilder.h
+++ tools/clang/lib/Sema/HLSLOutParamBuilder.h
@@ -96,7 +96,7 @@ public:
     // seen the decl before, generate a HLSLOutParamExpr that can't be elided.
     if (DF.MultipleFound || DF.Decl == nullptr ||
         DF.Decl->getType().getQualifiers().hasAddressSpace() ||
-        SeenVars.count(DF.Decl) > 0 || DF.Decl->hasExternalStorage())
+        SeenVars.count(DF.Decl) > 0 || !DF.Decl->hasLocalStorage())
       return ExprResult(
           HLSLOutParamExpr::Create(Ctx, Ty, Base, P->hasAttr<HLSLInOutAttr>()));
     // Add the decl to the seen list, and generate a HLSLOutParamExpr that can
