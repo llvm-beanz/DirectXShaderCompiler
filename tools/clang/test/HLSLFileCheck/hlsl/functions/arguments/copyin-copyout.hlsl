e21e46c6333acb48250d4c78bb29ca6f5fb2b3e7
Chris Bieneman <cbieneman@microsoft.com>
2023-04-21 11:34:21 -0500

    Reverse writeback order
    Under the MSVC ABI arguments are emitted right to left, but writebacks
    need to be emitted left to right for HLSL.
    
    This is probably a case that we need to look into more.

--- tools/clang/test/HLSLFileCheck/hlsl/functions/arguments/copyin-copyout.hlsl
+++ tools/clang/test/HLSLFileCheck/hlsl/functions/arguments/copyin-copyout.hlsl
@@ -16,11 +16,11 @@ void fn() {
 }
 
 // CHECK: define internal void @"\01?fn{{[@$?.A-Za-z0-9_]+}}"()
-// CHECK: [[Tmp1:%[0-9a-z.]+]] = alloca float
-// CHECK: [[Tmp2:%[0-9a-z.]+]] = alloca float
 // CHECK: [[X:%[0-9A-Z]+]] = alloca float, align 4
 // CHECK: [[Y:%[0-9A-Z]+]] = alloca float, align 4
 // CHECK: [[Z:%[0-9A-Z]+]] = alloca float, align 4
+// CHECK: [[Tmp1:%[0-9a-z.]+]] = alloca float
+// CHECK: [[Tmp2:%[0-9a-z.]+]] = alloca float
 
 // First call has no copy-in/copy out parameters since all parameters are unique.
 // CHECK: call void @"\01?CalledFunction
@@ -28,24 +28,24 @@ void fn() {
 
 // Second call, copies X for parameter 2.
 // CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[X]], align 4
-// CHECK: store float [[TmpX]], float* [[Tmp2]]
+// CHECK: store float [[TmpX]], float* [[Tmp1]]
 
 // CHECK: call void @"\01?CalledFunction
-// CHECK-SAME: (float* dereferenceable(4) [[X]], float* dereferenceable(4) [[Tmp2]], float* dereferenceable(4) [[Z]])
+// CHECK-SAME: (float* dereferenceable(4) [[X]], float* dereferenceable(4) [[Tmp1]], float* dereferenceable(4) [[Z]])
 
 // Second call, saves parameter 2 to X after the call.
-// CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[Tmp2]]
+// CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[Tmp1]]
 // CHECK: store float [[TmpX]], float* [[X]]
 
 // The third call copies X for the third parameter.
 // CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[X]], align 4
-// CHECK: store float [[TmpX]], float* [[Tmp1]]
+// CHECK: store float [[TmpX]], float* [[Tmp2]]
 
 // CHECK: call void @"\01?CalledFunction
-// CHECK-SAME: (float* dereferenceable(4) [[X]], float* dereferenceable(4) [[Y]], float* dereferenceable(4) [[Tmp1]])
+// CHECK-SAME: (float* dereferenceable(4) [[X]], float* dereferenceable(4) [[Y]], float* dereferenceable(4) [[Tmp2]])
 
 // The third call stores parameter 3 to X after the call.
-// CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[Tmp1]]
+// CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[Tmp2]]
 // CHECK: store float [[TmpX]], float* [[X]]
 
 // CHECK: ret
@@ -57,15 +57,16 @@ void fn2() {
 
 // CHECK: define internal void @"\01?fn2
 
+// CHECK: [[X:%[0-9A-Z]+]] = alloca float, align 4
 // CHECK: [[Tmp1:%[0-9a-z.]+]] = alloca float
 // CHECK: [[Tmp2:%[0-9a-z.]+]] = alloca float
-// CHECK: [[X:%[0-9A-Z]+]] = alloca float, align 4
 
-// X gets copied in for both parameters two and three.
-// CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[X]], align 4
-// CHECK: store float [[TmpX]], float* [[Tmp2]]
+// X gets copied in for both parameters two and three. The MSVC ABI dictates
+// right to left construction of arguments.
 // CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[X]], align 4
 // CHECK: store float [[TmpX]], float* [[Tmp1]]
+// CHECK: [[TmpX:%[0-9A-Z]+]] = load float, float* [[X]], align 4
+// CHECK: store float [[TmpX]], float* [[Tmp2]]
 
 // CHECK: call void @"\01?CalledFunction
 // CHECK-SAME: (float* dereferenceable(4) [[X]], float* dereferenceable(4) [[Tmp2]], float* dereferenceable(4) [[Tmp1]])
@@ -74,7 +75,7 @@ void fn2() {
 // the final value of X.
 
 // CHECK: [[X1:%[0-9A-Z]+]] = load float, float* [[Tmp2]]
-// CHECK: store float [[X1]], float* [[X]], align 4
+// CHECK: store float [[X1]], float* [[X]]
 
 // CHECK: [[X2:%[0-9A-Z]+]] = load float, float* [[Tmp1]]
 // CHECK: store float [[X2]], float* [[X]]
