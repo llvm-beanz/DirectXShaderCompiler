0c17f619f664ae630a65521ec4ff5836af56569a
Chris Bieneman <cbieneman@microsoft.com>
2023-07-05 12:50:21 -0500

    Fixing failing test case
    The old test verified incorrect debug info based on incorrect code
    generation. This test verifies correct debug info.
    
    ../tools/clang/test/HLSLFileCheck/dxil/debug/out_args.hlsl

--- tools/clang/test/HLSLFileCheck/dxil/debug/out_args.hlsl
+++ tools/clang/test/HLSLFileCheck/dxil/debug/out_args.hlsl
@@ -1,15 +1,30 @@
 // RUN: %dxc -E main -T ps_6_0 %s -Zi -O0 | FileCheck %s
 
 // CHECK-NOT: DW_OP_deref
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"arg0" !DIExpression(DW_OP_bit_piece, 0, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"arg0" !DIExpression(DW_OP_bit_piece, 32, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"arg0" !DIExpression(DW_OP_bit_piece, 64, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"arg1" !DIExpression(DW_OP_bit_piece, 0, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"arg1" !DIExpression(DW_OP_bit_piece, 32, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"arg1" !DIExpression(DW_OP_bit_piece, 64, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"output" !DIExpression(DW_OP_bit_piece, 0, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"output" !DIExpression(DW_OP_bit_piece, 32, 32)
-// CHECK-DAG: dbg.value(metadata float {{.+}}, i64 0, metadata !{{[0-9]+}}, metadata !{{[0-9]+}}), !dbg !{{[0-9]+}} ; var:"output" !DIExpression(DW_OP_bit_piece, 64, 32)
+
+// arg0 gets initialized in foo to {1,2,3}.
+// CHECK: call void @llvm.dbg.value(metadata <3 x float> <float 1.000000e+00, float 2.000000e+00, float 3.000000e+00>, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"arg0" !DIExpression() func:"foo"
+
+// output gets initialized in main to {1,2,3} on the return of foo.
+// CHECK: call void @llvm.dbg.value(metadata <3 x float> <float 1.000000e+00, float 2.000000e+00, float 3.000000e+00>, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"output" !DIExpression() func:"main"
+
+// arg1 gets initialized in main to {1,2,3} by copying from output before the
+// call to bar.
+// CHECK: call void @llvm.dbg.value(metadata <3 x float> <float 1.000000e+00, float 2.000000e+00, float 3.000000e+00>, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"arg1" !DIExpression() func:"bar"
+
+// arg1 gets updated in bar to {2, 4, 6}.
+// CHECK: call void @llvm.dbg.value(metadata float 2.000000e+00, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"arg1" !DIExpression(DW_OP_bit_piece, 0, 32) func:"bar"
+// CHECK-NEXT: call void @llvm.dbg.value(metadata float 4.000000e+00, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"arg1" !DIExpression(DW_OP_bit_piece, 32, 32) func:"bar"
+// CHECK-NEXT: call void @llvm.dbg.value(metadata float 6.000000e+00, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"arg1" !DIExpression(DW_OP_bit_piece, 64, 32) func:"bar"
+
+// arg1 gets copied back to output on the return of bar, setting output to {2,
+// 4, 6}.
+// CHECK: call void @llvm.dbg.value(metadata float 2.000000e+00, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"output" !DIExpression(DW_OP_bit_piece, 0, 32) func:"main"
+// CHECK-NEXT: call void @llvm.dbg.value(metadata float 4.000000e+00, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"output" !DIExpression(DW_OP_bit_piece, 32, 32) func:"main"
+// CHECK-NEXT: call void @llvm.dbg.value(metadata float 6.000000e+00, i64 0, metadata {{![0-9]+}}, metadata {{![0-9]+}}), !dbg {{![0-9]+}} ; var:"output" !DIExpression(DW_OP_bit_piece, 64, 32) func:"main"
+
+// CHECK-NOT: DW_OP_deref
+// CHECK: !llvm.dbg.cu
 
 void foo(out float3 arg0) {
   arg0 = float3(1,2,3); // @BREAK
